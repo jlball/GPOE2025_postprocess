2b6ab9486fca1e6cc0efb111ff2c5c8ee300b2d8
John Ball <jlball@mit.edu>
2025-01-18 10:12:26 -0500

    Added option to use multiprocessing to accelerate processing

--- timelapse_maker.py
+++ timelapse_maker.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 import progressbar as pb
 import numpy as np
+from multiprocessing import Pool
 
 parser = argparse.ArgumentParser()  
 parser.add_argument("dir", help="directory with hdf5 files")
@@ -15,12 +16,12 @@ parser.add_argument("--timestamp", "-ts", help="draw timestamp on each video fra
 parser.add_argument("--resize", "-r", help="resize images to this height", type=int)
 parser.add_argument("--font_size", "-fs", help="font size for timestamp", type=int, default=48)
 parser.add_argument("--camera_name", "-cn", help="camera name to display on video")
+parser.add_argument("--output_dir", "-o", help="output directory for video", default="timelapses")
+parser.add_argument("--multiprocessing", "-mp", help="use multiprocessing", action="store_true")
 args = parser.parse_args()
 
 timestamps, images = read_files(args.dir, 'exposures')
 print("Finished loading image data...")
-
-#images = images[:, :, 7:-7, :]
 
 datetimes = [datetime.fromtimestamp(ts) for ts in timestamps]
 
@@ -33,12 +34,8 @@ if args.resize is not None:
 
 font = ImageFont.truetype("Arial.ttf",args.font_size)
 
-# Initialize progress bar
-bar = pb.ProgressBar(max_value=len(images))
-
-# Loop through all images and apply selected processing steps
-for i, image in enumerate(bar(images)):
-    pil_image = Image.fromarray(image)
+def process_image(i):
+    pil_image = Image.fromarray(images[i])
 
     if args.resize is not None:
         pil_image = pil_image.resize(new_size)
@@ -58,13 +55,31 @@ for i, image in enumerate(bar(images)):
         position = (25, pil_image.height - text_height - 25)
         draw.text(position, text, font=font, fill="white")
 
-    processed_images.append(np.array(pil_image))
+    return np.array(pil_image)
+
+bar = pb.ProgressBar(max_value=len(images))
+
+# Loop through all images and apply selected processing steps
+if args.multiprocessing:
+    with Pool() as pool:
+        #processed_images = []
+        for result in bar(pool.imap(process_image, range(len(images)))):
+            processed_images.append(result)
+
+else:
+    for i, image in enumerate(bar(images)):
+        processed_images.append(process_image(i))
 
 images = np.array(processed_images)
 
+if args.output_dir == "timelapses" and args.camera_name is not None:
+    output_dir = args.camera_name
+else:
+    output_dir = args.output_dir
+
 try:
-    os.mkdir("timelapses")
+    os.mkdir(output_dir)
 except:
     pass
 
-imageio.mimsave(f'timelapses/{args.name}.mov', images, fps=float(args.fps))
+imageio.mimsave(f'{output_dir}/{args.name}.mov', images, fps=float(args.fps))
