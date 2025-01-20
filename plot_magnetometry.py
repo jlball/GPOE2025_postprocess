changes to plotting with consistent x axis across subplots and fix to derivatives

plot_magnetometry.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import medfilt, savgol_filter
 from scipy.interpolate import make_smoothing_spline
+import scipy.ndimage as nd
 
 def plot_mag_curve(folder_name, ax, color="red", filter_kernel=15, camera_name=None, derivative=False): 
     files = [
@@ -36,7 +37,7 @@ def plot_mag_curve(folder_name, ax, color="red", filter_kernel=15, camera_name=N
     bys -= bys[0]
     bzs -= bzs[0]
 
-    b2s = np.array(bxs)**2 + np.array(bys)**2 + np.array(bzs)**2
+    b2s = np.sqrt(np.array(bxs)**2 + np.array(bys)**2 + np.array(bzs)**2)
 
     times_hrs = (timestamps - timestamps[0])/3600
 
@@ -45,47 +46,88 @@ def plot_mag_curve(folder_name, ax, color="red", filter_kernel=15, camera_name=N
     else:
         label = folder_name
 
-    if derivative:
-        spline_fit_x = make_smoothing_spline(times_hrs, bxs, lam=4)
-        spline_fit_y = make_smoothing_spline(times_hrs, bxs, lam=4)
-        spline_fit_z = make_smoothing_spline(times_hrs, bxs, lam=4)
+    lam = 1e-6
+    spline_fit_x = (make_smoothing_spline(times_hrs, nd.median_filter(bxs, 5), lam=lam)(times_hrs))
+    spline_fit_y = (make_smoothing_spline(times_hrs, nd.median_filter(bys, 5), lam=lam)(times_hrs))
+    spline_fit_z = (make_smoothing_spline(times_hrs, nd.median_filter(bzs, 5), lam=lam)(times_hrs))
+
+    dxdt = nd.median_filter(np.gradient(spline_fit_x), 5)
+    dydt = nd.median_filter(np.gradient(spline_fit_y), 5)
+    dzdt = nd.median_filter(np.gradient(spline_fit_z), 5)
+
+    ax[1].step(times_hrs, 
+        dxdt,
+        where='mid', 
+        label=label,
+        color='red')
+    ax[1].step(times_hrs, 
+        dydt,
+        where='mid', 
+        label=label,
+        color='blue')
+    ax[1].step(times_hrs, 
+        dzdt,
+        where='mid', 
+        label=label,
+        color='green')
+    
+    ax[1].set_ylabel('Field Strength Change (uT/step)')
 
 
-        # ax2 = ax.twinx()
+    # ax2 = ax.twinx()
 
-        # ax2.step(times_hrs, 
-        #     medfilt(temps, kernel_size=filter_kernel), 
-        #     where='mid', 
-        #     color='black')
+    # ax2.step(times_hrs, 
+    #     medfilt(temps, kernel_size=filter_kernel), 
+    #     where='mid', 
+    #     color='black')
 
-        # ax2.plot(times_hrs,
-        #     spline_fit(times_hrs), color="green")
+    # ax2.plot(times_hrs,
+    #     spline_fit(times_hrs), color="green")
 
-    else:
-        ax.step(times_hrs, 
-            medfilt(bxs, kernel_size=filter_kernel), 
-            where='mid', 
-            label=label,
-            color='red')
-        ax.step(times_hrs, 
-            medfilt(bys, kernel_size=filter_kernel), 
-            where='mid', 
-            label=label,
-            color='blue')
-        ax.step(times_hrs, 
-            medfilt(bzs, kernel_size=filter_kernel), 
-            where='mid', 
-            label=label,
-            color='green')
-        ax.step(times_hrs, 
-            medfilt(b2s, kernel_size=filter_kernel), 
-            where='mid', 
-            label=label,
-            color='black')
-        ax.set_ylabel('Field Strength (uT)')
+    ax[0].step(times_hrs, 
+        medfilt(bxs, kernel_size=filter_kernel), 
+        where='mid', 
+        label=label,
+        color='red', 
+        alpha = .1)
+    ax[0].step(times_hrs, 
+        medfilt(bys, kernel_size=filter_kernel), 
+        where='mid', 
+        label=label,
+        color='blue', 
+        alpha = .1)
+    ax[0].step(times_hrs, 
+        medfilt(bzs, kernel_size=filter_kernel), 
+        where='mid', 
+        label=label,
+        color='green', 
+        alpha =.1)
+    ax[0].step(times_hrs, 
+        medfilt(b2s, kernel_size=filter_kernel), 
+        where='mid', 
+        label=label,
+        color='black', 
+        alpha = .1)
 
-    ax.set_xlabel('Time (hrs)')
-    ax.set_xbound(0, times_hrs[-1])
+    ax[0].step(times_hrs, 
+        spline_fit_x,
+        where='mid', 
+        label=label,
+        color='red')
+    ax[0].step(times_hrs, 
+        spline_fit_y,
+        where='mid', 
+        label=label,
+        color='blue')
+    ax[0].step(times_hrs, 
+        spline_fit_z,
+        where='mid', 
+        label=label,
+        color='green')
+    ax[0].set_ylabel('Field Strength (uT)')
+
+    ax[1].set_xlabel('Time (hrs)')
+    ax[1].set_xbound(0, times_hrs[-1])
 
     return ax
 
@@ -97,9 +139,12 @@ if __name__ == "__main__":
     parser.add_argument("--output_name", help="output name", default='temperature.png')
     args = parser.parse_args()
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(2, sharex = True)
+    ax[0].grid()
+    ax[1].grid()
+    plt.subplots_adjust(wspace=0, hspace=0)
 
-    ax = plot_mag_curve(args.dir, ax, color="red", filter_kernel=args.filter_kernel)
+    ax = plot_mag_curve(args.dir, ax, color="red", filter_kernel=args.filter_kernel,derivative = True)
 
     #plt.show()  # Add this line to render the plot
 